2237563001ab9bc51d2d928703a6eaaf768da839
Supriyo Das <supriyod026@gmail.com>
2026-08-04 01:19:43 +0530

    feat(frontend): setup auth services and state management

---
+++ frontend/src/services/auth.service.ts
@@ -0,0 +1,9 @@
+import { api } from "../lib/api";
+
+export async function login(data: {
+  email: string;
+  password: string;
+}) {
+  const response = await api.post("/auth/login", data);
+  return response.data;
+}
